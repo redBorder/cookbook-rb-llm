aaa2eb7f265811d2534ba6937acb1d6b40dc800d
Pablo Pérez <pperez@redborder.com>
2025-02-06 10:37:18 +0000

    Rubocop made me do this

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -61,7 +61,7 @@ action :add do
           service_needs_restart = true
         elsif Dir.exist?(dir_path) && !Dir.empty?(dir_path)
           if ::File.symlink?(symlink_path) && ::File.readlink(symlink_path) == target_path
-            Chef::Log.info("Symlink already points to the correct model, skipping update.")
+            Chef::Log.info('Symlink already points to the correct model, skipping update.')
           else
             Chef::Log.info("#{dir_path} is not empty, triggering update_ai_model")
             resources(execute: 'update_ai_model').run_action(:run)
@@ -158,7 +158,7 @@ action :register do
         'ID' => "redborder-ai-#{node['hostname']}",
         'Name' => 'redborder-ai',
         'Address' => ipaddress,
-        'Port' => 50505
+        'Port' => 50505,
       }
       json_query = Chef::JSONCompat.to_json(query)
 
